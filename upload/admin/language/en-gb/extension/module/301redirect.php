fc6e73c533c354ac650ab97831f733a6fe6354e0
Tarcísio Ruas Ferraz <tarcisioruas@gmail.com>
2018-05-19 13:43:35 -0300

    Adjusting english language file

--- upload/admin/language/en-gb/extension/module/301redirect.php
+++ upload/admin/language/en-gb/extension/module/301redirect.php
@@ -3,34 +3,34 @@
 $_['heading_title']          = '301 Redirect';
 
 // Text
-$_['text_success']           = 'Sucesso: Você modificou o redirecionamento!';
-$_['text_list']              = 'Redirecionamentos';
-$_['text_config']            = 'Editando 301 Redirect';
-$_['text_add']               = 'Adicionar Redirecionamento';
-$_['text_edit']              = 'Editar Redirecionamento';
-$_['text_default']           = 'Padrão';
-$_['text_extension']         = 'Extensões';
-$_['text_module_disabled']   = 'O módulo está desativado, para editar a lista habilite-o';
+$_['text_success']           = 'Success: You changed the redirect!';
+$_['text_list']              = 'Redirects';
+$_['text_config']            = 'Editing 301 Redirect';
+$_['text_add']               = 'Add Redirect';
+$_['text_edit']              = 'Edit Redirect';
+$_['text_default']           = 'Default';
+$_['text_extension']         = 'Extensions';
+$_['text_module_disabled']   = 'This module is disabled. To edit any redirect, you need to enable it';
 
 
 // Column
-$_['column_url_from']        = 'Origem';
-$_['column_url_to']          = 'Destino';
-$_['column_action']          = 'Ações';
+$_['column_url_from']        = 'URL From';
+$_['column_url_to']          = 'URL To';
+$_['column_action']          = 'Actions';
 
 
 // Entry
-$_['entry_url_from']         = 'Origem';
-$_['entry_url_to']           = 'Destino';
-$_['entry_status']           = 'Situação';
+$_['entry_url_from']         = 'URL From';
+$_['entry_url_to']           = 'URL To';
+$_['entry_status']           = 'Status';
 
 
 // Help
 $_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_url_from_empty']       = 'Url de origem deve ser informada.';
-$_['error_url_to_empty']         = 'Url de destino deve ser informada.';
-$_['error_url_from_exists']      = 'Uma Ulr de origem assim como essa já foi cadastrada anteriormente.';
-$_['error_url_to_dont_exists']   = 'Url de destino não existe.';
-$_['error_permission']           = 'Alerta: Você não tem permissão para editar esse redirecionamento!';
+$_['error_url_from_empty']       = 'The URL From is required.';
+$_['error_url_to_empty']         = 'The URL To is required.';
+$_['error_url_from_exists']      = 'This URL From was registrated already.';
+$_['error_url_to_dont_exists']   = 'This URL To don\'t exist.';
+$_['error_permission']           = 'Warning: You haven\'t permission to edit this redirect!';
