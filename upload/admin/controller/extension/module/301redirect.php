510775f98085fddc09eaf3bedf1222f8469d75fb
Tarcísio Ruas Ferraz <tarcisioruas@gmail.com>
2018-06-07 19:38:24 -0300

    Fixing bug at default list order

--- upload/admin/controller/extension/module/301redirect.php
+++ upload/admin/controller/extension/module/301redirect.php
@@ -263,7 +263,7 @@ class ControllerExtensionModule301redirect extends Controller {
 		if (isset($this->request->get['sort'])) {
 			$sort = $this->request->get['sort'];
 		} else {
-			$sort = 'id.title';
+			$sort = 'id';
 		}
 
 		if (isset($this->request->get['order'])) {
